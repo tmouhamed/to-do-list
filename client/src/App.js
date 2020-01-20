662da3cb0fe99adcc41959088f17b95d33e1f0c8
tmouhamed <s.toqaa@gmail.com>
2020-01-20 16:34:03 -0500

    add controlled form to Todoinput component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Axios from 'axios';
 import uuid from 'uuid';
 import './App.css';
+import TodoInput from './components/TodoInput';
 
 class App extends React.Component {
   url = 'http://localhost:8080';
@@ -28,27 +29,33 @@ class App extends React.Component {
 
   postNewTask = () => {
     const taskToPost = {
-        title: this.state.taskTitle,
-        description: this.state.taskDescription,
-        date: this.state.taskDate,
-        status: this.state.taskStatus,
-        category: this.state.taskCategory,
+      title: this.state.taskTitle,
+      description: this.state.taskDescription,
+      date: this.state.taskDate,
+      status: this.state.taskStatus,
+      category: this.state.taskCategory,
     }
     Axios.post(`${this.url}/list/`, taskToPost)
-        .then(response => {
-          console.log(response.data);
-          
-        })
-        .catch(
-            error => console.error("This is an error", error
-            ));
-}
+      .then(response => {
+        console.log(response.data);
+
+      })
+      .catch(
+        error => console.error("This is an error", error
+        ));
+  }
   componentDidMount() {
     this.getTaskList();
   }
   render() {
     return (
-      <div className="App">
+      <div className="container">
+        <div class="card border-info">
+          <h4 class="card-header bg-info text-white text-center"> To-Do List</h4>
+          <div class="card-body">
+            <TodoInput taskStatus={this.state.taskStatus} taskCategory={this.state.taskCategory} taskDate={this.state.taskDate} handleChange={this.handleChange} handleSubmit={this.handleSubmit} taskTitle={this.state.taskTitle} taskDescription={this.state.taskDescription} editTask={this.state.editTask} changeDate={this.changeDate} selectedDate={this.state.taskDate} />
+          </div>
+        </div>
       </div>
     );
   }
---
+++ client/src/components/TodoInput.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import DatePicker from "react-datepicker";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarWeek } from '@fortawesome/free-solid-svg-icons';
+
+import "react-datepicker/dist/react-datepicker.css";
+
+class TodoInput extends React.Component {
+    render() {
+        const { handleChange, handleSubmit, taskDescription, taskTitle, editTask, changeDate, taskStatus, taskDate, taskCategory } = this.props;
+        return (
+            <div className="my-3 card card-body">
+                <form onSubmit={handleSubmit} className="needs-validation">
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label htmlFor="taskTitle">Title</label>
+                            <input className="form-control text-capitalize" type="text" placeholder="Title" name="taskTitle" value={taskTitle} onChange={handleChange} required />
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label htmlFor="textarea1">Descritpion</label>
+                            <textarea className="form-control" id="textarea1" rows="1" type="text" placeholder="Descritpion" name="taskDescription" value={taskDescription} onChange={handleChange}></textarea>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-4">
+                            <label htmlFor="statuSelect1">Task Status</label>
+                            <select className="form-control" id="statuSelect1" name="taskStatus" value={taskStatus} onChange={handleChange} required>
+                                <option value="">select</option>
+                                <option value="pending">Pending</option>
+                                <option value="active">Active</option>
+                                <option value="done">Done !</option>
+                            </select>
+                        </div>
+                        <div class="form-group col-md-3">
+                            <label htmlFor="statuSelect2">Task Category</label>
+                            <select className="form-control" id="statuSelect2" name="taskCategory"  value={taskCategory} onChange={handleChange} required>
+                                <option value="">select</option>
+                                <option value="work">Work</option>
+                                <option value="personal">Personal</option>
+                                <option value="health">Health</option>
+                                <option value="health">Meeting</option>
+                                <option value="health">Study</option>
+                            </select>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label>Date</label>
+                            <div className="input-group">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text">
+                                        <FontAwesomeIcon icon={faCalendarWeek} />
+                                    </span>
+                                </div>
+                                <DatePicker className="form-control" onChange={changeDate} isClearable showTimeSelect selected={taskDate} dateFormat="MMMM d, yyyy h:mm aa" />
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" className={editTask ? "btn btn-block btn-success mt-3" : "btn right btn-outline-info"}>{editTask ? "Edit Item" : "Add Item"}</button>
+                </form>
+            </div >
+        );
+    }
+
+}
+
+export default TodoInput;
